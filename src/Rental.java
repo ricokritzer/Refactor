2810c4bf486cd4051584927407cbf11ceb35ad32
Lukas Frederik Nonnenmacher <lukas-frederik.nonnenmacher@schneeberger.com>
2016-04-26 18:37:23 +0200

    Extract Method Die Aufgabe der Punktevergabe aus der Klasse Customer entfernt in Rental eine neue Funktion getfrequentRenterPoints hinzugefügt

--- src/Rental.java
+++ src/Rental.java
@@ -30,4 +30,12 @@ class Rental {
     	 }
     	return result;
     }
+    public int getfrequentRenterPoints(){
+    	if((getMovie().getPriceCode()== Movie.NEW_RELEASE)&& getDaysRented()>1){
+    		return 2;
+    	}
+    	else{
+    		return 1;
+    	}
+    }
 }
