425c2f870852d8ec685807295a5f46ba91144013
Lukas Frederik Nonnenmacher <lukas-frederik.nonnenmacher@schneeberger.com>
2016-04-27 19:30:01 +0200

    Replace Type Code with State/strategy für Price eigene Klassen erstellt (mit Vererbung) die Zugriffe dafür im Movie geändert. Erstellte Klassen: Price (abstract) ChildrensPrice RegularPrice NewReleasePrice

--- src/Movie.java
+++ src/Movie.java
@@ -3,16 +3,28 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price _price;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return _price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg){
+        	case REGULAR:
+        		_price = new RegularPrice();
+        		break;
+        	case CHILDRENS:
+        		_price = new ChildrensPrice();
+        		break;
+        	case NEW_RELEASE:
+        		_price = new NewReleasePrice();
+        		break;
+        	default:
+        		throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     public String getTitle (){
         return title;
