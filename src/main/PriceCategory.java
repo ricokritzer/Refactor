f7d4735a0c2c526f211453db981957b0498e17e1
K3 <K3@R-K-07>
2018-05-05 20:22:21 +0200

    Berechnung optimiert

--- src/main/PriceCategory.java
+++ src/main/PriceCategory.java
@@ -2,21 +2,24 @@ package main;
 
 public enum PriceCategory
 {
-	REGULAR(2, 1.5, 2, 1), //
-	NEW_RELEASE(0, 3, 0, 0), //
-	CHILDREN(1.5, 1.5, 3, 3);
+	REGULAR(2, 1.5, 2, 1, 1), //
+	NEW_RELEASE(0, 3, 0, 0, 2), //
+	CHILDREN(1.5, 1.5, 3, 3, 1);
 
 	private double grundpreis = 0;
 	private double aufpreisFaktor = 0;
 	private int aufpreisAbTag = 0;
 	private int rabatt;
 
-	private PriceCategory(double grundpreis, double aufpreis, int aufpreisAbTag, int rabatt)
+	private int renterPointsGreaterOne = 1;
+
+	private PriceCategory(double grundpreis, double aufpreis, int aufpreisAbTag, int rabatt, int renterPointsGreaterOne)
 	{
 		this.grundpreis = grundpreis;
 		this.aufpreisFaktor = aufpreis;
 		this.aufpreisAbTag = aufpreisAbTag;
 		this.rabatt = rabatt;
+		this.renterPointsGreaterOne = renterPointsGreaterOne;
 	}
 
 	public double getCharge(int days)
@@ -33,18 +36,13 @@ public enum PriceCategory
 		return category.getCharge(days);
 	}
 
+	public int getFrequentRenterPoints(int daysRented)
+	{
+		return daysRented > 1 ? renterPointsGreaterOne : 1;
+	}
+
 	public static int getFrequentRenterPoints(PriceCategory cat, int daysRented)
 	{
-		switch (cat)
-		{
-			case CHILDREN:
-				return 1;
-			case NEW_RELEASE:
-				return (daysRented > 1) ? 2 : 1;
-			case REGULAR:
-				return 1;
-			default:
-				return 1;
-		}
+		return cat.getFrequentRenterPoints(daysRented);
 	}
 }
