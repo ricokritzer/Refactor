5b69496d2d99188a6783b7c6c16eac57107d4f98
K3 <K3@R-K-07>
2018-05-05 19:42:01 +0200

    Reordering

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -2,9 +2,10 @@ package main;
 
 public class Movie
 {
-	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
+	public static final int CHILDRENS = 2;
+
 	private String title;
 	private Price price;
 
