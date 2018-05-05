24ff6f89f35709e38e370c15db5eb59f952f277f
K3 <K3@R-K-07>
2018-05-05 19:11:04 +0200

    Extracted Method and inline

--- src/main/Program.java
+++ src/main/Program.java
@@ -13,8 +13,7 @@ public class Program
 
 	public static void main(String args[])
 	{
-		String result;
-		System.out.println("Welcome to the Movie Store");
+		printLn("Welcome to the Movie Store");
 		Movie m1 = new Movie("movie1", 1);
 		Movie m2 = new Movie("movie2", 2);
 		Rental r1 = new Rental(m1, 10);
@@ -22,8 +21,12 @@ public class Program
 		Customer c1 = new Customer("joe");
 		c1.addRental(r1);
 		c1.addRental(r2);
-		System.out.println("Let's get the Statement");
-		result = c1.statement();
-		System.out.println(result);
+		printLn("Let's get the Statement");
+		printLn(c1.statement());
+	}
+
+	public static void printLn(String ausgabe)
+	{
+		System.out.println(ausgabe); // NOSONAR
 	}
 }
